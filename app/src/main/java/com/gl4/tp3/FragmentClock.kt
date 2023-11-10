88616a48efeb4f146b0a12bd029750cc331f7ddd
ons-ou <onsouahchi@gmail.com>
2023-11-10 19:52:44 +0100

    first commit

---
+++ app/src/main/java/com/gl4/tp3/FragmentClock.kt
@@ -0,0 +1,29 @@
+package com.gl4.tp3
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+private const val IS_DIGITAL = "digitalOK"
+class FragmentClock : Fragment() {
+    private var digitalClock = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            digitalClock = it.getBoolean(IS_DIGITAL)
+        }
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return if (!digitalClock)
+            inflater.inflate(R.layout.fragment_digital, container, false)
+        else
+            inflater.inflate(R.layout.fragment_numeric, container, false)
+    }
+}
---
+++ app/src/main/java/com/gl4/tp3/MainActivity.kt
@@ -0,0 +1,104 @@
+package com.gl4.tp3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ActionMode
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.isVisible
+import com.gl4.tp3.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), ActionMode.Callback {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var actionMode: ActionMode
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment,FragmentClock(),null)
+            .addToBackStack(null)
+            .commit()
+
+        // code à ajouter
+        binding.setButton.setOnLongClickListener{
+            actionMode = this@MainActivity.startActionMode(this@MainActivity)!!
+            return@setOnLongClickListener true
+        }
+
+        binding.setButton.setOnClickListener {
+            setTime(it)
+        }
+        val toolbar: Toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.action_switch)
+        {
+            binding.switchButton.isChecked = !binding.switchButton.isChecked
+            setTime(null)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun setTime(view: View?)
+    {
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        val fragmentClock = FragmentClock()
+        val bundle = Bundle()
+
+        println(binding.switchButton.isChecked)
+        bundle.putBoolean("digitalOK", binding.switchButton.isChecked)
+        fragmentClock.arguments = bundle
+        transaction.replace(R.id.fragment,fragmentClock)
+        transaction.commit()
+    }
+
+    override fun onCreateActionMode(actionMode: ActionMode, menu: Menu?): Boolean {
+        val inflater: MenuInflater = actionMode.menuInflater
+        inflater.inflate(R.menu.context_mode_menu, menu)
+        binding.headerBar.visibility = View.GONE
+        return true
+    }
+
+    override fun onPrepareActionMode(p0: ActionMode?, p1: Menu?): Boolean {
+        return true
+    }
+
+    override fun onActionItemClicked(actionMode: ActionMode?, menuItem: MenuItem?): Boolean {
+        return when (menuItem?.itemId) {
+            R.id.action_color -> {
+                binding.setButton.setBackgroundColor(
+                    resources.getColor(
+                        R.color.black
+                    )
+                )
+
+                actionMode?.finish()
+
+                true
+            }
+            else -> false
+        }
+    }
+
+    override fun onDestroyActionMode(p0: ActionMode?) {
+        binding.headerBar.visibility = View.VISIBLE
+    }
+
+}
+
